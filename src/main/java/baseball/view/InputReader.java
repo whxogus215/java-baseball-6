a456caf046a1c39e51423c886dea6ddeeb10dfce
whxogus215 <whxogus215@naver.com>
2023-11-30 23:35:42 +0900

    refactor: InputView -> InputReader로 변경

--- src/main/java/baseball/view/InputReader.java
+++ src/main/java/baseball/view/InputReader.java
@@ -2,11 +2,8 @@ package baseball.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class InputView {
-    private static final String INPUT = "숫자를 입력해주세요 : ";
-
+public class InputReader {
     public static String readInput() {
-        System.out.println(INPUT);
         return Console.readLine();
     }
 }
