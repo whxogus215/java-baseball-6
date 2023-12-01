fd2b270f8b68d742b9792c55a64a9445ab3d8dd6
whxogus215 <whxogus215@naver.com>
2023-12-01 22:34:53 +0900

    feat: 결과 출력 포맷 추가

--- src/main/java/baseball/dto/ResultType.java
+++ src/main/java/baseball/dto/ResultType.java
@@ -2,30 +2,33 @@ package baseball.dto;
 
 import java.util.Arrays;
 import java.util.function.BiFunction;
-import java.util.function.Predicate;
 
 public enum ResultType {
 
-    ALL_STRIKE(true, "게임 세트 종료", (ball, strike) -> ball == 0 && strike == 3),
-    BALL_AND_STRIKE(false, "볼과 스트라이크 존재",
+    ALL_STRIKE(true, "3스트라이크", (ball, strike) -> ball == 0 && strike == 3),
+    BALL_AND_STRIKE(false, "%d볼 %d스트라이크",
             (ball, strike) -> (ball > 0 && ball < 3) && (strike > 0 && strike < 3)),
-    ONLY_STRIKE(false, "스트라이크만 존재", (ball, strike) -> ball == 0 && (strike > 0 && strike < 3)),
-    ONLY_BALL(false, "볼만 존재", (ball, strike) -> (ball > 0 && ball <= 3) && strike == 0),
+    ONLY_STRIKE(false, "%d스트라이크", (ball, strike) -> ball == 0 && (strike > 0 && strike < 3)),
+    ONLY_BALL(false, "%d볼", (ball, strike) -> (ball > 0 && ball <= 3) && strike == 0),
     NOTHING(false, "낫싱", (ball, strike) -> ball == 0 && strike == 0);
 
     private boolean isEnd;
-    private String detail;
+    private String printFormat;
     private BiFunction<Integer, Integer, Boolean> resultTypeFunction;
 
     ResultType(boolean isEnd, String detail,
                BiFunction<Integer, Integer, Boolean> resultTypeFunction) {
         this.isEnd = isEnd;
-        this.detail = detail;
+        this.printFormat = detail;
         this.resultTypeFunction = resultTypeFunction;
     }
 
     public boolean isEnd() {
         return isEnd;
+    }
+
+    public String getPrintFormat() {
+        return printFormat;
     }
 
     public static ResultType findResultTypeByCount(Integer countOfBall, Integer countOfStrike) {
