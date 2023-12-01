8f27e136e73db7d2e407c6195d0d7cae10027bc0
whxogus215 <whxogus215@naver.com>
2023-12-01 22:42:23 +0900

    feat: 결과타입에 따른 출력 메서드 구현

--- src/main/java/baseball/view/output/ResultOutputWriter.java
+++ src/main/java/baseball/view/output/ResultOutputWriter.java
@@ -1,8 +1,31 @@
 package baseball.view.output;
 
-import baseball.config.Message;
+import baseball.dto.Result;
+import baseball.dto.ResultType;
 
 public class ResultOutputWriter extends OutputWriter {
+    private ResultOutputWriter() {
+
+    }
+
+    public static void printResult(Result result) {
+        ResultType resultType = result.getResultType();
+        int countOfBall = result.getCountOfBall();
+        int countOfStrike = result.getCountOfStrike();
+        if (resultType.equals(ResultType.BALL_AND_STRIKE)) {
+            println(String.format(resultType.getPrintFormat(), countOfBall, countOfStrike));
+        }
+        if (resultType.equals(ResultType.ONLY_STRIKE)) {
+            println(String.format(resultType.getPrintFormat(), countOfStrike));
+        }
+        if (resultType.equals(ResultType.ONLY_BALL)) {
+            println(String.format(resultType.getPrintFormat(), countOfBall));
+        }
+        if (resultType.equals(ResultType.ALL_STRIKE) || resultType.equals(ResultType.NOTHING)) {
+            println(resultType.getPrintFormat());
+        }
+    }
+
 
 
 
