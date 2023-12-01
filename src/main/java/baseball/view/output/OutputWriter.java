582b1a688ae815b6a13e142484b9df6dc0a1e35b
whxogus215 <whxogus215@naver.com>
2023-12-01 22:04:00 +0900

    feat: OutputWriter 구현

---
+++ src/main/java/baseball/view/output/OutputWriter.java
@@ -0,0 +1,18 @@
+package baseball.view.output;
+
+import baseball.config.Message;
+
+public class OutputWriter {
+
+    public static void println(Object object) {
+        System.out.println(object);
+    }
+
+    public static void printNewLine() {
+        System.out.println();
+    }
+
+    public static void printMessage(Message message) {
+        println(message.getDetail());
+    }
+}
---
+++ src/main/java/baseball/view/output/ResultOutputWriter.java
@@ -0,0 +1,9 @@
+package baseball.view.output;
+
+import baseball.config.Message;
+
+public class ResultOutputWriter extends OutputWriter {
+
+
+
+}
