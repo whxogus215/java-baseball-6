1909838f5b204f97ade4fc6e41d3cc3ac06f7fd1
whxogus215 <whxogus215@naver.com>
2023-12-01 22:50:18 +0900

    feat: 게임 종료 메시지 출력 메서드 구현

--- src/main/java/baseball/view/output/ResultOutputWriter.java
+++ src/main/java/baseball/view/output/ResultOutputWriter.java
@@ -4,6 +4,8 @@ import baseball.dto.Result;
 import baseball.dto.ResultType;
 
 public class ResultOutputWriter extends OutputWriter {
+    private static final String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
     private ResultOutputWriter() {
 
     }
@@ -24,6 +26,10 @@ public class ResultOutputWriter extends OutputWriter {
         if (resultType.equals(ResultType.ALL_STRIKE) || resultType.equals(ResultType.NOTHING)) {
             println(resultType.getPrintFormat());
         }
+    }
+
+    public static void printEnding() {
+        println(END_MESSAGE);
     }
 
 
