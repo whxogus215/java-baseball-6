a537e7c706a89b3ffaa4b3e91f1534155ec961dd
whxogus215 <whxogus215@naver.com>
2023-12-01 22:09:25 +0900

    feat: 결과 타입 추가

--- src/main/java/baseball/dto/ResultType.java
+++ src/main/java/baseball/dto/ResultType.java
@@ -3,6 +3,10 @@ package baseball.dto;
 public enum ResultType {
 
     ALL_STRIKE(true, "게임 세트 종료"),
+    BALL_AND_STRIKE(false, "볼과 스트라이크 존재"),
+    ONLY_STRIKE(false, "스트라이크만 존재"),
+    ONLY_BALL(false, "볼만 존재"),
+    NOTHING(false, "낫싱"),
     NOT_END(false, "게임 계속 진행");
 
     private boolean isEnd;
